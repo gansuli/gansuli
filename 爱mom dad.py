0139bc41d54f384ac25e4b90e1d26bd6ffacfae5
gansuli <1757363273@qq.com>
2018-05-27 20:38:04 +0800

    实验报告10-224

--- 爱mom dad.py
+++ 爱mom dad.py
@@ -4,7 +4,7 @@ Created on Sun May 20 19:45:41 2018
 
 @author: asus
 """
-print('\n'.join([''.join([('Love dad'[(x-y)%8]
+print('\n'.join([''.join([('dad LOVE'[(x-y)%8]
 if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 
 else' ')
 for x in range(-30,30)])
